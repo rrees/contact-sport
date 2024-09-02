6f1a00cef4eafeb1a38be05294eab0b64036b693
Robert Rees <shudderfix@gmail.com>
2024-09-02 20:26:33 +0100

    Creates a working deployment with Basic Auth

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+ARG PYTHON_VERSION=3.11.9
+
+FROM python:${PYTHON_VERSION}-slim-bookworm AS base
+
+RUN apt update && apt upgrade -y
+
+FROM base AS builder
+
+ENV PYTHONUNBUFFERED=1 \
+    PYTHONDONTWRITEBYTECODE=1 \
+    PIPENV_VENV_IN_PROJECT=1 \
+    PIPENV_CUSTOM_VENV_NAME=.venv
+
+WORKDIR /app
+COPY . .
+
+RUN pip install pipenv && pipenv install
+
+FROM base
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+COPY --from=builder /app .
+
+RUN pip install pipenv
+
+CMD [ "pipenv", "run", "gunicorn", "--bind=0.0.0.0:8080", "--worker-tmp-dir", "/dev/shm",   "app.app:app"]
--- app/app.py
+++ app/app.py
@@ -20,7 +20,8 @@ app.secret_key = os.environ.get("SECRET_KEY", os.urandom(24))
 app.logger.setLevel(logging.INFO)
 
 routes = [
-    ("/", "index", handlers.pages.front_page, ["GET"]),
+    # ("/", "index", handlers.pages.front_page, ["GET"]),
+    ("/", "index", handlers.pages.home_page, ["GET"]),
     ("/home", "home", handlers.pages.home_page, ["GET"]),
     ("/contacts", "contacts", handlers.pages.contacts, ["GET"]),
 ]
@@ -28,7 +29,25 @@ routes = [
 for path, endpoint, handler, methods in routes:
     app.add_url_rule(path, endpoint, handler, methods=methods)
 
-app.register_blueprint(passwordless_blueprint)
+# app.register_blueprint(passwordless_blueprint)
+
+
+@app.before_request
+def before_request():
+    def check_credentials(username, password):
+        valid_users = os.environ.get("VALID_USERS").split(",")
+        return username in valid_users and password == os.environ.get("ADMIN_PASSWORD")
+
+    auth = flask.request.authorization
+    if (
+        not auth
+        or not auth.username
+        or not auth.password
+        or not check_credentials(auth.username, auth.password)
+    ):
+        return flask.Response(
+            "Please login", 401, {"WWW-Authenticate": 'Basic realm="contact-sport"'}
+        )
 
 
 @app.errorhandler(500)
