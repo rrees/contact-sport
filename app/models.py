ea824a202419fffa11c4195ee51605d48ee9448e
Robert Rees <shudderfix@gmail.com>
2024-10-13 09:48:55 +0100

    Includes the emails on the contact screen

--- app/models.py
+++ app/models.py
@@ -11,11 +11,22 @@ class Address(NamedTuple):
         return Address(external_id, label, address)
 
 
+class Email(NamedTuple):
+    external_id: str
+    label: str
+    email: str
+
+    def from_list(l):
+        [external_id, label, email] = l
+        return Email(external_id, label, email)
+
+
 class Contact(NamedTuple):
     external_id: str
     name: str
     tags: list[str]
     addresses: Optional[list[Address]]
+    emails: Optional[list[Email]]
 
     @staticmethod
     def from_dict(a_dict):
@@ -24,4 +35,5 @@ class Contact(NamedTuple):
             a_dict["name"],
             a_dict["tags"],
             [Address.from_list(a) for a in a_dict.get("addresses", [])],
+            [Email.from_list(e) for e in a_dict.get("emails", [])],
         )
